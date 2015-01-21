caa88d51c669ff87fd59466773edd30f88aa76e8
Mario Zorica <zorica.mario@rocketmail.com>
2015-01-21 16:49:07 +0100

    v1.0.0.4 Added unit test to cover all currently supported conversion types.

--- MadMilkman.Ini.Tests/IniFileMiscellaneousTests.cs
+++ MadMilkman.Ini.Tests/IniFileMiscellaneousTests.cs
@@ -24,17 +24,47 @@ namespace MadMilkman.Ini.Tests
             file.Sections["Section"].Keys["bool"].TryParseValue(out resultingBoolean);
             Assert.AreEqual(true, resultingBoolean);
 
+            var numberKey = file.Sections["Section"].Keys["number"];
+
+            byte resultingByte;
+            numberKey.TryParseValue(out resultingByte);
+            Assert.AreEqual((byte)10, resultingByte);
+
+            sbyte resultingSignedByte;
+            numberKey.TryParseValue(out resultingSignedByte);
+            Assert.AreEqual((sbyte)10, resultingSignedByte);
+
+            short resultingShort;
+            numberKey.TryParseValue(out resultingShort);
+            Assert.AreEqual((short)10, resultingShort);
+
+            ushort resultingUnsignedShort;
+            numberKey.TryParseValue(out resultingUnsignedShort);
+            Assert.AreEqual((ushort)10, resultingUnsignedShort);
+
             int resultingInteger;
-            file.Sections["Section"].Keys["number"].TryParseValue(out resultingInteger);
+            numberKey.TryParseValue(out resultingInteger);
             Assert.AreEqual(10, resultingInteger);
 
+            uint resultingUnsignedInteger;
+            numberKey.TryParseValue(out resultingUnsignedInteger);
+            Assert.AreEqual(10U, resultingUnsignedInteger);
+
+            long resultingLong;
+            numberKey.TryParseValue(out resultingLong);
+            Assert.AreEqual(10L, resultingLong);
+
             ulong resultingUnsignedLong;
-            file.Sections["Section"].Keys["number"].TryParseValue(out resultingUnsignedLong);
-            Assert.AreEqual(10ul, resultingUnsignedLong);
+            numberKey.TryParseValue(out resultingUnsignedLong);
+            Assert.AreEqual(10UL, resultingUnsignedLong);
+
+            float resultingSingle;
+            numberKey.TryParseValue(out resultingSingle);
+            Assert.AreEqual(10F, resultingSingle);
 
             double resultingDouble;
-            file.Sections["Section"].Keys["number"].TryParseValue(out resultingDouble);
-            Assert.AreEqual(10d, resultingDouble);
+            numberKey.TryParseValue(out resultingDouble);
+            Assert.AreEqual(10D, resultingDouble);
 
             DateTime resultingDateTime;
             file.Sections["Section"].Keys["date"].TryParseValue(out resultingDateTime);
